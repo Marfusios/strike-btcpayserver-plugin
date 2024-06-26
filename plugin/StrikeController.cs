b85d1f8b0583a5aacfbdf1850d7f348ee99e0b40
Mariusz Kotas <m@mkotas.cz>
2024-06-26 15:00:58 +0200

    Update config page and libs

--- plugin/StrikeController.cs
+++ plugin/StrikeController.cs
@@ -15,49 +15,49 @@ namespace BTCPayServer.Plugins.Strike;
 [Route("plugins/{storeId}/strike")]
 public class StrikeController : Controller
 {
-    private readonly BTCPayNetworkProvider _btcPayNetworkProvider;
-    private readonly BTCPayWalletProvider _btcWalletProvider;
-    private readonly StoreRepository _storeRepository;
+	private readonly BTCPayNetworkProvider _btcPayNetworkProvider;
+	private readonly BTCPayWalletProvider _btcWalletProvider;
+	private readonly StoreRepository _storeRepository;
 
-    public StrikeController(BTCPayNetworkProvider btcPayNetworkProvider,
-        BTCPayWalletProvider btcWalletProvider, StoreRepository storeRepository)
-    {
-        _btcPayNetworkProvider = btcPayNetworkProvider;
-        _btcWalletProvider = btcWalletProvider;
-        _storeRepository = storeRepository;
-    }
+	public StrikeController(BTCPayNetworkProvider btcPayNetworkProvider,
+		BTCPayWalletProvider btcWalletProvider, StoreRepository storeRepository)
+	{
+		_btcPayNetworkProvider = btcPayNetworkProvider;
+		_btcWalletProvider = btcWalletProvider;
+		_storeRepository = storeRepository;
+	}
 
 
-    [HttpGet("")]
-    public IActionResult Index(string storeId)
-    {
-        return RedirectToAction(nameof(Dashboard), new {storeId});
-    }
+	[HttpGet("")]
+	public IActionResult Index(string storeId)
+	{
+		return RedirectToAction(nameof(Dashboard), new { storeId });
+	}
 
-    [HttpGet("dashboard")]
-    [Authorize(Policy = Policies.CanViewStoreSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
-    public IActionResult Dashboard(string storeId)
-    {
-	    return View((object)storeId);
-    }
-    
-    [Authorize(Policy = Policies.CanModifyStoreSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
-    [HttpGet("configure")]
-    public IActionResult Configure(string storeId)
-    {
-        return View();
-    }
+	[HttpGet("dashboard")]
+	[Authorize(Policy = Policies.CanViewStoreSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+	public IActionResult Dashboard(string storeId)
+	{
+		return View((object)storeId);
+	}
 
-    [HttpPost("configure")]
-    [Authorize(Policy = Policies.CanModifyStoreSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
-    public IActionResult Configure(string storeId, string command, string settings)
-    {
-        var store = HttpContext.GetStoreData();
-        var existing = store.GetSupportedPaymentMethods(_btcPayNetworkProvider).OfType<LightningSupportedPaymentMethod>()
-            .FirstOrDefault(method =>
-                method.PaymentId.PaymentType == LightningPaymentType.Instance &&
-                method.PaymentId.CryptoCode == "BTC");
+	[Authorize(Policy = Policies.CanModifyStoreSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+	[HttpGet("configure")]
+	public IActionResult Configure(string storeId)
+	{
+		return View();
+	}
 
-        return RedirectToAction(nameof(Configure), new {storeId});
-    }
+	[HttpPost("configure")]
+	[Authorize(Policy = Policies.CanModifyStoreSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+	public IActionResult Configure(string storeId, string command, string settings)
+	{
+		var store = HttpContext.GetStoreData();
+		var existing = store.GetSupportedPaymentMethods(_btcPayNetworkProvider).OfType<LightningSupportedPaymentMethod>()
+			.FirstOrDefault(method =>
+				method.PaymentId.PaymentType == LightningPaymentType.Instance &&
+				method.PaymentId.CryptoCode == "BTC");
+
+		return RedirectToAction(nameof(Configure), new { storeId });
+	}
 }
