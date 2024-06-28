bb73500888f78e3698acf823e2157abf01a9861c
Mariusz Kotas <m@mkotas.cz>
2024-06-28 15:06:33 +0200

    Database migration: payments

---
+++ plugin/Migrations/20240628123640_Payments.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BTCPayServer.Plugins.Strike.Migrations
+{
+    /// <inheritdoc />
+    public partial class Payments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                schema: "BTCPayServer.Plugins.Strike",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TenantId = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    PaymentId = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    LightningInvoice = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    PaymentHash = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CompletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    RequestedBtcAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    TargetAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    TargetCurrency = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    RealBtcFeeAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    FeeAmount = table.Column<decimal>(type: "numeric", nullable: true),
+                    FeeCurrency = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    ConversionRate = table.Column<decimal>(type: "numeric", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments",
+                schema: "BTCPayServer.Plugins.Strike");
+        }
+    }
+}
